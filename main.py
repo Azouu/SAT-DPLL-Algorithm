01a5e5f71fab45036e5750460df2608962c91152
Azou <pika.azou@gmail.com>
2021-03-11 14:52:44 +0100

    add clauses_unitaires = looks like it is working

--- main.py
+++ main.py
@@ -28,44 +28,55 @@ def find_litteral(X, clause) :
             return litteral
     return None
 
+def simplifier_clause(S, clause) :
+    res = []
+    for c in clause :
+        for s in S :
+            if abs(c) == s[0] :
+                val = c * s[1]
+                if val > 0 :
+                    res.append(c)
+    return res
+
 def test_consistance(C, S) :
     consistent = True
-    clauses_unitaires = []
     i = 0
     while i < len(C) and consistent :
         nb_false = 0
-        nb_litteraux = len(C[i])
-        c = None
+        tmp = C[i].copy()
         for affectation in S :
             X = affectation[0]
             v = affectation[1]
             litteral = find_litteral(X, C[i])
+            # Si la variable est dans la clause
             if litteral :
                 if litteral * v < 0 :
-                    nb_false = nb_false + 1
-                else :
-                    c = litteral
-        if nb_false == nb_litteraux:
+                    tmp.remove(litteral)
+        if len(tmp) == 0 :
             consistent = False
-        elif nb_false == nb_litteraux - 1 :
-            if c  :
-                clauses_unitaires.append(c)
+            return []
         i = i + 1
-    return consistent, clauses_unitaires
+    return tmp
 
-def next_var_to_set(S, liste_variables) :
-    affected_variables = []
-    for s in S:
-        affected_variables.append(s[0])
+def variables_affectees(S) :
+    variables_affectees = []
+    for affectation in S :
+        variables_affectees.append(affectation[0])
+    return variables_affectees
 
-    var = [x for x in liste_variables if x not in affected_variables][0]
-    return var
-
-def choix_affectation(X, valeurs) :
-    if len(valeurs) == 0 :
-        return (X, 1, -1)
-    if len(valeurs) == 1 :
-        return (X, valeurs[0], - valeurs[0])
+def choix_variable(variables, S, clauses_pas_affectees_faux) :
+    variables_aff = variables_affectees(S)
+    if len(clauses_pas_affectees_faux) == 1 :
+        for c in clauses_pas_affectees_faux :
+            if abs(c) not in variables_aff :
+                if c * 1 > 0 :
+                    return (abs(c), 1, None)
+                else :
+                    return (abs(c), -1, None)
+    for c in variables :
+        if c not in variables_aff :
+            return (c, 1, -1)
+    return None
 
 
 def dpll(variables, clauses) :
@@ -75,18 +86,19 @@ def dpll(variables, clauses) :
     nb_echecs = 0
     while not done:
         res_test_consistance =  test_consistance(clauses, S)
-        if res_test_consistance[0]:
+        if len(res_test_consistance) > 0:
             if len(S) == n:
                 done = True
             else:
-                S.append((next_var_to_set(S, variables), 1, -1))
-        else:
+                var = choix_variable(variables, S, res_test_consistance)
+                S.append(var)
+        else :
             nb_echecs = nb_echecs + 1
             affectation = S.pop()
             while len(S) > 0 and affectation[2] is None:
                 affectation = S.pop()
             if affectation[2] is not None:
-                S.append((affectation[0], -1, None))
+                S.append((affectation[0], affectation[1] * (-1), None))
             else:
                 done = True
     print(nb_echecs)
@@ -95,15 +107,14 @@ def dpll(variables, clauses) :
 
 if __name__ == '__main__':
     clauses, variables = read_cnf('uf20-01.cnf')
-    c = [[1,-2, 4], [-3,4], [-1,-3]]
-    s = [(1,1,-1), (2,+1,-1), (3,-1,None),(4,1,-1)]
-    a = test_consistance(c, s)
-    print(a)
+    #c = [[1,-2, 4], [-3,4], [-1,-3]]
+    #s = [(1,1,-1), (2,+1,-1), (3,1,-1)]
+    #a = test_consistance(c, s)
+    #print(a)
 
     start_time = time.time()
     a = dpll(variables, clauses)
-    print(a)
-    print(time.time() - start_time)
+    print("Temps d'execution de dpll : {}".format(time.time() - start_time))
     print(a)
     exit(0)
 
